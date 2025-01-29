268809e5caf5bf43e30fcad1279f8c81c7c2a28e
Umayanga12 <umayangaathapaththu05@gmail.com>
2025-01-29 11:48:49 +0530

    document loader

---
+++ DocumentLoader.py
@@ -0,0 +1,10 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+print(all_splits[0])
