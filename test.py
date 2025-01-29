ca6fec06f0710b97dd3ba7d36b5dcbf940bd2de8
Umayanga12 <umayangaathapaththu05@gmail.com>
2025-01-29 14:42:48 +0530

    test script

---
+++ test.py
@@ -0,0 +1,125 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+
+local_embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=local_embeddings)
+
+question = "What are the approaches to Task Decomposition?"
+docs = vectorstore.similarity_search(question)
+# print(len(docs))
+#print(docs)
+from langchain_ollama import ChatOllama
+
+model = ChatOllama(
+    model="llama3.1:8b",
+)
+
+response_message = model.invoke(
+    "Simulate a rap battle between Stephen Colbert and John Oliver"
+)
+
+# print(response_message.content)
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+prompt = ChatPromptTemplate.from_template(
+    "Summarize the main themes in these retrieved docs: {docs}"
+)
+
+
+# Convert loaded documents into strings by concatenating their content
+# and ignoring metadata
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+chain = {"docs": format_docs} | prompt | model | StrOutputParser()
+
+question = "What are the approaches to Task Decomposition?"
+
+docs = vectorstore.similarity_search(question)
+chain.invoke(docs)
+
+from langchain_core.runnables import RunnablePassthrough
+
+RAG_TEMPLATE = """
+You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+
+<context>
+{context}
+</context>
+
+Answer the following question:
+
+{question}"""
+
+rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
+
+chain = (
+    RunnablePassthrough.assign(context=lambda input: format_docs(input["context"]))
+    | rag_prompt
+    | model
+    | StrOutputParser()
+)
+
+question = "What are the approaches to Task Decomposition?"
+
+docs = vectorstore.similarity_search(question)
+
+# Run
+chain.invoke({"context": docs, "question": question})
+
+
+from langchain_core.runnables import RunnablePassthrough
+
+RAG_TEMPLATE = """
+You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+
+<context>
+{context}
+</context>
+
+Answer the following question:
+
+{question}"""
+
+rag_prompt = ChatPromptTemplate.from_template(RAG_TEMPLATE)
+
+chain = (
+    RunnablePassthrough.assign(context=lambda input: format_docs(input["context"]))
+    | rag_prompt
+    | model
+    | StrOutputParser()
+)
+
+question = "What are the approaches to Task Decomposition?"
+
+docs = vectorstore.similarity_search(question)
+
+# Run
+chain.invoke({"context": docs, "question": question})
+
+retriever = vectorstore.as_retriever()
+
+qa_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | rag_prompt
+    | model
+    | StrOutputParser()
+)
+
+question = "What are the approaches to Task Decomposition?"
+
+qa_chain.invoke(question)
