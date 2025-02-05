26e9ddd662114df311d9bfc1a0c4bc7055997fa7
Umayanga12 <umayangaathapaththu05@gmail.com>
2025-02-05 10:09:57 +0530

    adding rag prompting

--- RAGTemplete.py
+++ RAGTemplete.py
@@ -1,12 +1,26 @@
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
-
+from langchain_core.output_parsers import StrOutputParser
 class RAGTemplete:
     """
     RAGTemplete
     """
-    def __init__(self, rag_template):
+    def __init__(self, rag_template, format_docs, model,question,vectorstore):
         self.rag_template = rag_template
+        self.format_docs = format_docs
+        self.model = model
+        self.question = question
+        self.vectorstore = vectorstore
 
     def RagPrompt(self):
-        return ChatPromptTemplate.from_template(self.rag_template)
+        ragprompt =  ChatPromptTemplate.from_template(self.rag_template)
+        chain = (
+            RunnablePassthrough.assign(context=lambda input: self.format_docs(input["context"]))
+            | self.rag_prompt
+            | self.model
+            | StrOutputParser()
+        )
+        docs = self.vectorstore.similarity_search(self.question)
+        chain.invoke({"context": docs, "question": self.question})
+        return self.vectorstore.as_retriever()
+
